503c878ef018756aeac0c0c8f7d370caca85d096
Ellyn Bouscasse <ellyn.bouscasse@gmail.com>
2018-06-19 21:41:54 +0200

    modifying carts and carts items

--- app/models/order.rb
+++
@@ -1,4 +0,0 @@
-class Order < ApplicationRecord
-  has_many :reviews
-  has_many :order_items
-end
--- app/models/order_item.rb
+++
@@ -1,5 +0,0 @@
-class OrderItem < ApplicationRecord
-  belongs_to :user
-  belongs_to :product
-  belongs_to :order, optional: true
-end
