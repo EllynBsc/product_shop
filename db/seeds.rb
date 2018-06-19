8ec71d996f21e5301c107c9a4c27f02fbb3a83e1
Ellyn Bouscasse <ellyn.bouscasse@gmail.com>
2018-06-19 21:08:35 +0200

    products controller view routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.destroy_all
+
+p "starting"
+
+product1 = Product.create!(title: "wood straws", unit_price: 5, description: "amazing", stock: 7)
+
+product2 = Product.create!(title: "bamboo straws", unit_price: 4, description: "ecological as fuck", stock: 10)
+
+product3 = Product.create!(title: "metal straws", unit_price: 6, description: "cold", stock: 12)
+
+product4 = Product.create!(title: "fake plastic straws", unit_price: 14, description: "plastikkk?", stock: 22)
+
+p "seed finished!"
